03f706775ea63ddeb082377a53b6babdeaef35f1
Kushikiari-Eris <erispena303@gmail.com>
2024-10-15 18:58:48 +0800

    uploads

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "*",
+    origin: 'http://localhost:5173',
     credentials: true,
 }))
 dotenv.config()
